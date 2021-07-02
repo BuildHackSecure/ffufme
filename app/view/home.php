98f616599f281c6b14961a7c2a072e11a7caf195
adam <adamtlangley@gmail.com>
2021-07-03 00:27:21 +0100

    bit of a pointless update just to test my update script

--- app/view/home.php
+++ app/view/home.php
@@ -1,12 +1,9 @@
 <?php View::page('header'); ?>
     <h2 style="margin: 0;padding: 0">For FFUF Target Practice</h2>
     <div class="text-center"><a href="/about">About</a> | <a href="/help">Help</a> | <a href="/credits">Credits</a></div>
-
     <div class="welcome text-center" style="margin-top:20px;background-color: #e76c6c">
         <p>Before you start make sure you download the required <a href="/wordlists">wordlists</a> first</p>
     </div>
-
-
     <h3>Content Discovery</h3>
     <ul>
         <li><a href="/cd/basic">Basic Content Discovery</a></li>
@@ -21,5 +18,4 @@
     <ul>
         <li><a href="/sub/vhost">Virtual Host Discovery</a></li>
     </ul>
-
 <?php View::page('footer'); ?>
