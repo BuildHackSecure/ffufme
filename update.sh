f5b6d8a3a1662b8a32fae85b1fe403d1fec028bf
adam <adamtlangley@gmail.com>
2021-07-14 11:03:57 +0100

    Updating my automation script

--- update.sh
+++ update.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+#This script is just to support the http://ffuf.me infra and isn't required for any personal usage
+
+ip=$(ifconfig eth1 | grep 'inet ' | cut -d' ' -f10)
 current_git=$(git rev-parse HEAD)
 git pull
 now_git=$(git rev-parse HEAD)
 if [[ "$current_git" != "$now_git" ]]; then
     docker build -t ffufme .
-    docker kill $(docker ps -q)
-    docker run -d -p 80:80 ffufme
+    sleep $(shuf -i 10-30 -n 1)
+    docker kill $(docker ps --filter ancestor=ffufme -q)
+    docker run -d -p $ip:10000:80 ffufme
 fi
